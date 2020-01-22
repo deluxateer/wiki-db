9ee4262bdec9a8bf6172d303f2dfa14dee51ecd4
Deluxateer <deluxateer@gmail.com>
2020-01-22 13:38:35 -0600

    add delete /articles/:articleTitle endpoint

--- app.js
+++ app.js
@@ -98,6 +98,15 @@ app.route('/articles/:articleTitle')
         }
       }
     );
+  })
+  .delete(function(req, res) {
+    Article.deleteOne({ title: req.params.articleTitle }, function(err) {
+      if(!err) {
+        res.send('Article successfully deleted.');
+      } else {
+        res.send(err);
+      }
+    });
   });
 
 app.listen(3000, function() {
