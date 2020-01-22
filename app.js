856ef75c2db7fec5bbfa38bd92008f0466685ebe
Deluxateer <deluxateer@gmail.com>
2020-01-22 13:31:06 -0600

    add put & patch /articles/:articleTitle routes

--- app.js
+++ app.js
@@ -73,6 +73,31 @@ app.route('/articles/:articleTitle')
         res.send('No articles matching that title was found.');
       }
     });
+  })
+  .put(function(req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      function(err) {
+        if(!err) {
+          res.send('Successfully updated articles.');
+        }
+      }
+    );
+  })
+  .patch(function(req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function(err) {
+        if(!err) {
+          res.send('Successfully updated articles.');
+        } else {
+          res.send(err);
+        }
+      }
+    );
   });
 
 app.listen(3000, function() {
